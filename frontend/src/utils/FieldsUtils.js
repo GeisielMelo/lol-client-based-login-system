31582812e67f73398f1296943d7ef869d08d075a
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-08-25 12:43:15 -0300

    feat: Created FieldsUtils module to handle field verification in the Sign component

---
+++ frontend/src/utils/FieldsUtils.js
@@ -0,0 +1,17 @@
+export const isEmpty = (fields) => {
+  if (fields.some((field) => !field || field.trim() === "")) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const isEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(String(email).toLowerCase())) {
+    return true;
+  } else {
+    return false;
+  }
+};
