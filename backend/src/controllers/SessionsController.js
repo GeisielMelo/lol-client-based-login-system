4cb6de1e89df72999c22b958d6b69c8acff1255d
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-08-29 10:33:13 -0300

    docs: Removed console logs from the codebase.

--- backend/src/controllers/SessionsController.js
+++ backend/src/controllers/SessionsController.js
@@ -48,7 +48,6 @@ class SessionsController {
         }),
       });
     } catch (error) {
-      console.log(error);
       return res.status(500).json({ error: "Fail to create a session." });
     }
   }
